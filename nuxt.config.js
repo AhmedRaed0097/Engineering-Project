102e7b610c05963cc1e067ae659829c569633468
AhmedRaed0097 <83637337+AhmedRaed0097@users.noreply.github.com>
2022-04-06 23:31:56 +0300

    initial  commit

---
+++ nuxt.config.js
@@ -0,0 +1,102 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+
+  ssr: false,
+// test
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - attendance-pwa-app',
+    title: 'attendance-pwa-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    // { src: '~/plugins/qrCode', mode: 'client', ssr: false },
+  ],
+
+// server:{
+//   host:'0.0.0.0',
+//   port:3000
+// },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+    // 'nuxt-vite'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    // baseURL: 'http://192.168.55.120:8000/api/',
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  // pwa: {
+  //   manifest: {
+  //     lang: 'en',
+  //   },
+  // },
+  pwa: {
+    meta: {
+      title: 'Engineering-App',
+      author: 'Ahmed Raed',
+    },
+    manifest: {
+      name: 'Engineering-App',
+      short_name: 'Engineering-App',
+      lang: 'ar',
+      display: 'standalone'
+    },
+    workbox: {
+      cachingExtensions: '@/plugins/workbox-sync.js',
+      enabled: false //should be off actually per workbox docs due to complications when used in prod
+    }
+  },
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
