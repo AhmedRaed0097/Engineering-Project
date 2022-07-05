e53f76ef05993ad52805eafeb3399e99b3c48ad2
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-07-05 21:00:49 +0300

    change port number

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,10 +31,10 @@ export default {
     // { src: '~/plugins/qrCode', mode: 'client', ssr: false },
   ],
 
-// server:{
-//   host:'0.0.0.0',
-//   port:3000
-// },
+server:{
+  host:'0.0.0.0',
+  port:5000
+},
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
